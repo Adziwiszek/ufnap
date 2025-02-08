a0a53435f9fab5d6057d7a48f9b3b9f5a605ca1d
AleksandraPonikowska <150003323+AleksandraPonikowska@users.noreply.github.com>
2025-02-08 17:20:36 +0100

    Logowanie (#6)
    * Added basic loging page
    
    * Changed color
    
    * Added some loging
    
    * Added some registering
    
    * Multiversum (#5)
    
    Added ability to add multiple scenes and to add teleports that take player from one scene to another. Not everything is perfect right now, but I will make more improvements.
    
    * added testlobbyscene that inherits from worldscene
    
    * added grass tileset for background
    
    * scaled tileset and fixed issue with chat bubble not showing
    
    * added teleporter object
    
    * there is 'working' teleporter but it is very buggy
    
    * moved sockets to session manager
    
    * fixed clients player sprite tint
    
    * working...
    
    * player can, without an error, teleport to another scene
    
    * when a player joins existing players can see them
    
    * switched chat to use session manager
    
    * fixed chat messages to work with sessionManager
    
    * fixed issue with adding new players
    
    * fixed player disconnecting
    
    * going to another scene works correctly now!
    
    * smol fixes
    
    * Added some loging
    
    * fixed css link
    
    ---------
    
    Co-authored-by: Adziwiszek <64317423+Adziwiszek@users.noreply.github.com>
    Co-authored-by: Adam Dziwiszek <adziwiszek1@gmail.com>

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,8 +13,81 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.static(path.join(__dirname, '../client/html')));
 app.use(express.static(path.join(__dirname, '../client/js')));
 
+// Logowanie
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.use(session({
+  secret: 'klucz',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/html/index.html'));  
+});
+
+// Obsługa logowania
+app.post('/login', (req, res) => {
+
+  const users = [
+    { login: "ola", password: bcrypt.hashSync("123", 10) }
+  ];
+
+  const { login, password } = req.body;
+
+  const user = users.find(u => u.login === login);
+  if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = login;
+      return res.redirect('/game');
+  } else {
+    return res.send(`
+      <script>
+        alert('Błędny login lub hasło!');
+        window.location.href = "/";
+      </script>
+    `);
+  }
+});
+
+//Obsługa rejestracji
+app.post('/register', (req, res) => {
+  const { login, password, password2} = req.body;
+
+  // TODO: check if user with that nick already exists
+
+  // if (false) { 
+  //   return res.send(`
+  //     <script>
+  //       alert('Użytkownik o tym loginie już istnieje!');
+  //       window.location.href = "/";
+  //     </script>
+  //   `);
+  // }
+
+  if (password !== password2) {
+    return res.send(`
+      <script>
+        alert('Hasła nie są takie same!');
+        window.location.href = "/";
+      </script>
+    `);
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  return res.send(`
+    <script>
+      alert('`+login+hashedPassword+`');
+      window.location.href = "/";
+    </script>
+  `);
+});
+
+
+app.get('/game', (req, res) => {
+  res.redirect('game.html');
 });
 
 let players = {};
@@ -118,14 +193,10 @@ io.on('connection', (socket) => {
         player.currentRoom = newRoom;
         console.log(players);
 
-        // notify other players in this room
+      // notify other players in this room
         socket.broadcast.to(newRoom).emit('newPlayer', { id: socket.id, x: player.x, y: player.y });
-    });
-    // handling player updates
-    /*setInterval(() => {
-    io.emit('update players', players);
-    }, 1000/30);*/
-});
+  });
+
   
 server.listen(3000, () => {
     // var host = server.address().address;
